ae4f40546938039914ba59224b2b97bc9253449d
Irene Canuti <irene.canuti@gmail.com>
2016-12-14 13:51:38 +0000

    write the working production code and tests

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -4,4 +4,15 @@ describe 'fizzbuzz' do
   it 'returns "fizz" when passed 3' do
     expect(fizzbuzz(3)).to eq 'fizz'
   end
+  it 'returns "buzz" when passed 5' do
+    expect(fizzbuzz(5)).to eq 'buzz'
+  end
+
+  it 'returns "fizzbuzz" when passed 15' do
+    expect(fizzbuzz(15)).to eq 'fizzbuzz'
+  end
+
+  it 'returns "number" when passed number' do
+    expect(fizzbuzz(14)).to eq 14
+  end
 end
